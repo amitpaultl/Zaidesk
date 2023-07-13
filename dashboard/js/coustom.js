dac8559fd126df6fc69349feba186885a97a1bcd
Amit Paul <amitpaultl@gmail.com>
2023-07-13 20:06:17 +0600

    dashbord

---
+++ dashboard/js/coustom.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    $("#demo").percircle({
+        perdown: true,
+        secs: 14,
+        timeUpText: 'finally!',
+        reset: true,
+        // more options here
+    });
+
+})(jQuery);
